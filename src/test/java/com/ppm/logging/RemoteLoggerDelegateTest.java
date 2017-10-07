7c5264cad1dc92a1b94654dc469ac40433a84f4c
Pedro Oliveira <pedro.oliveira20@gmail.com>
2017-10-06 22:12:24 -0400

    Merge branch 'master' of github.com:pedrotoliveira/ppm-logging into develop
    Conflicts:
    	build.gradle
    	src/main/java/com/ppm/logging/AsLevelBuilder.java
    	src/main/java/com/ppm/logging/FileLogger.java
    	src/main/java/com/ppm/logging/FileLoggerDelegate.java
    	src/main/java/com/ppm/logging/FluentLogger.java
    	src/main/java/com/ppm/logging/LogDataBuilder.java
    	src/main/java/com/ppm/logging/LogDataHolder.java
    	src/main/java/com/ppm/logging/LogKeyValueBuilder.java
    	src/main/java/com/ppm/logging/LogKeyValueBuilderHolder.java
    	src/main/java/com/ppm/logging/LogOperations.java
    	src/main/java/com/ppm/logging/LoggerHolder.java
    	src/main/java/com/ppm/logging/LoggerService.java
    	src/main/java/com/ppm/logging/RemoteLogger.java
    	src/main/java/com/ppm/logging/RemoteLoggerDelegate.java
    	src/main/java/com/ppm/logging/Severity.java
    	src/test/java/com/ppm/logging/FileLoggerDelegateTest.java
    	src/test/java/com/ppm/logging/LogKeyValueBuilderTest.java
    	src/test/java/com/ppm/logging/RemoteLoggerDelegateTest.java

--- src/test/java/com/ppm/logging/RemoteLoggerDelegateTest.java
+++ src/test/java/com/ppm/logging/RemoteLoggerDelegateTest.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (C) 2017 pedrotoliveira
  *
  * This program is free software; you can redistribute it and/or
@@ -17,85 +17,17 @@
  */
 package com.ppm.logging;
 
-import org.apache.logging.log4j.core.Logger;
-import static org.mockito.Mockito.atLeastOnce;
-import static org.mockito.Mockito.verify;
-
-import org.junit.Before;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 /**
  * Unit tests of class RemoteLoggerDelegate
  *
- * @author Pedro T. Oliveira <pedro.oliveira20@gmail.com>
+ * @author Pedro T. Oliveira
  */
 @RunWith(MockitoJUnitRunner.class)
 public class RemoteLoggerDelegateTest {
 
     private RemoteLoggerDelegate delegate;
-
-    private Object message;
-
-    @Mock
-    private Logger logger;
-
-    /**
-     * @throws java.lang.Exception
-     */
-    @Before
-    public void setUp() throws Exception {
-        this.message = Object.class.cast("Something to log");
-        this.delegate = RemoteLoggerDelegate.create(logger);
-    }
-
-    @Test
-    public final void testLogInfo() {
-        delegate.logInfo(message);
-        verify(logger, atLeastOnce()).info(message);
-    }
-
-    @Test
-    public final void testLogWarn() {
-        delegate.logWarn(message);
-        verify(logger, atLeastOnce()).warn(message);
-    }
-
-    @Test
-    public final void testLogDebug() {
-        delegate.logDebug(message);
-        verify(logger, atLeastOnce()).debug(message);
-    }
-
-    @Test
-    public final void testLogError() {
-        delegate.logError(message);
-        verify(logger, atLeastOnce()).error(message);
-    }
-
-    @Test
-    public final void testLogKeyValue() {
-        delegate.logKey("key=").value("value").asInfo();
-        verify(logger, atLeastOnce()).info("key=value ");
-
-        delegate.logKey("key=").value("value").asDebug();
-        verify(logger, atLeastOnce()).debug("key=value ");
-
-        delegate.logKey("key=").value("value").asWarn();
-        verify(logger, atLeastOnce()).warn("key=value ");
-
-        delegate.logKey("key=").value("value").asError();
-        verify(logger, atLeastOnce()).error("key=value ");
-    }
-
-    @Test
-    public void testLogKey() {
-    }
-
-    @Test
-    public void testLogData() {
-    }
 
 }
