1710d3d5201cbd195e2ff7e6dec9ef62469c954d
pedrotoliveira <pedro.oliveira20@gmail.com>
2023-03-17 02:03:27 -0300

    fix: update dependencies

--- src/test/java/com/ppm/logging/RemoteLoggerDelegateTest.java
+++ src/test/java/com/ppm/logging/RemoteLoggerDelegateTest.java
@@ -19,7 +19,7 @@ package com.ppm.logging;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 /**
  * Unit tests of class RemoteLoggerDelegate
